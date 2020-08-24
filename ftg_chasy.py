6ba199f573eb973fb21433dd061489a4e0dfbefd
lamer112311 <66884920+lamer112311@users.noreply.github.com>
2020-08-24 19:24:55 +0300

    Add files via upload

---
+++ ftg_chasy.py
@@ -0,0 +1,67 @@
+from telethon import events
+import asyncio
+import os
+import sys
+a = 0
+
+@borg.on(events.NewMessage(pattern=r"\.h", outgoing=True))#🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("🕐")
+    await asyncio.sleep(0.5)
+    await event.edit("🕑")
+    await asyncio.sleep(0.5)
+    await event.edit("🕒..")
+    await asyncio.sleep(0.5)
+    await event.edit("🕓")
+    await asyncio.sleep(0.5)
+    await event.edit("🕕")
+    await asyncio.sleep(0.5)
+    await event.edit("🕖")
+    await asyncio.sleep(0.5)
+    await event.edit("🕗")
+    await asyncio.sleep(0.5)
+    await event.edit("🕘")
+    await asyncio.sleep(0.5)
+    await event.edit("🕙")
+    await asyncio.sleep(0.5)
+    await event.edit("🕐")
+    await asyncio.sleep(0.5)
+    await event.edit("🕑")
+    await asyncio.sleep(0.5)
+    await event.edit("🕒..")
+    await asyncio.sleep(0.5)
+    await event.edit("🕓")
+    await asyncio.sleep(0.5)
+    await event.edit("🕕")
+    await asyncio.sleep(0.5)
+    await event.edit("🕖")
+    await asyncio.sleep(0.5)
+    await event.edit("🕗")
+    await asyncio.sleep(0.5)
+    await event.edit("🕘")
+    await asyncio.sleep(0.5)
+    await event.edit("🕙")
+    await asyncio.sleep(0.5)
+    await event.edit("🕐")
+    await asyncio.sleep(0.5)
+    await event.edit("🕑")
+    await asyncio.sleep(0.5)
+    await event.edit("🕒..")
+    await asyncio.sleep(0.5)
+    await event.edit("🕓")
+    await asyncio.sleep(0.5)
+    await event.edit("🕕")
+    await asyncio.sleep(0.5)
+    await event.edit("🕖")
+    await asyncio.sleep(0.5)
+    await event.edit("🕗")
+    await asyncio.sleep(0.5)
+    await event.edit("🕘")
+    await asyncio.sleep(0.5)
+    await event.edit("🕙")
+    await asyncio.sleep(0.5)
+
+    
+    
