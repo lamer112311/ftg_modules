67b525133633c6d5e16e3f90587bc8fa24415b33
lamer112311 <66884920+lamer112311@users.noreply.github.com>
2020-09-02 10:51:27 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,30 @@
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.pr", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+
+    await event.edit("\n🌑🌑🌑🌑🌑🌑🌑🌑🌑🌕🌕🌕🌕🌕🌕🌑🌑🌕🌕🌕🌕🌕🌕🌑🌑🌕🌕🌑🌑🌕🌕🌑🌑🌕🌕🌑🌑🌕🌕🌑🌑🌕🌕🌑🌑🌕🌕🌑🌑🌕🌕🌑🌑🌕🌕🌑🌑🌑🌑🌑🌑🌑🌑🌑")
+    await asyncio.sleep(0.5)
+    await event.edit("💣💣")
+    await asyncio.sleep(0.5)
+    await event.edit("💣💣💣")
+    await asyncio.sleep(0.5)
+    await event.edit("💣💣💣💣")
+    await asyncio.sleep(0.5)
+    await event.edit("💣💣💣💣💣")
+    await asyncio.sleep(0.5)
+    await event.edit("💣💣💣💣")
+    await asyncio.sleep(0.5)
+    await event.edit("💣💣💣")
+    await asyncio.sleep(0.5)
+    await event.edit("💣💣")
+    await asyncio.sleep(0.5)
+    await event.edit("💣")
+    await asyncio.sleep(0.5)
+    await event.edit("\n💥💥💥💥💥 \n💥💥💥💥💥 \n💥💥💥💥💥 \n💥💥💥💥💥 \n💥💥💥💥💥") #▪️💥
